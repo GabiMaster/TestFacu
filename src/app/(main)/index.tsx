23b88c27732f98381d3aa0f093706cd74175fc35
GabiMaster <martinezgabriel7007@gmail.com>
2025-06-17 11:58:29 -0300

    Inicialización de la app y ruteo

---
+++ src/app/(main)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Ingresamos a la aplicación</Text>
+    </View>
+  );
+};
+
+export default Main;
---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Redirect, Stack } from 'expo-router';
+import React, { useState } from 'react';
+
+const RootNavigation = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false}}></Stack>
+      {isLogin ? <Redirect href="/(main)" /> : <Redirect href="/(auth)" />}
+    </>
+  );
+};
+
+export default RootNavigation;
